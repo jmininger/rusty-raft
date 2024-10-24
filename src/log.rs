76823a30f26b67246df52ac0c2ba9eaffda67b0c
Jacquin Mininger <jacquin.mininger@gmail.com>
2024-10-24 11:20:59 -0400

    chore: comment unused code from two years ago; this will eventually get refactored back in, in a different form

--- src/log.rs
+++ src/log.rs
@@ -1,39 +1,39 @@
-use crate::utils::*;
+// use crate::utils::*;
 
-#[derive(Debug, Clone)]
-pub struct Log(Vec<LogEntry>);
+// #[derive(Debug, Clone)]
+// pub struct Log(Vec<LogEntry>);
 
-impl Log {
-    // Consider making htis a HashMap keyed on LogIndex
-    // Alternatively create a VecWithOffset type that allows me to index a compacted log with its
-    // actual index
-    pub fn new() -> Self {
-        Log(Vec::new())
-    }
+// impl Log {
+//     // Consider making htis a HashMap keyed on LogIndex
+//     // Alternatively create a VecWithOffset type that allows me to index a compacted log with its
+//     // actual index
+//     pub fn new() -> Self {
+//         Log(Vec::new())
+//     }
 
-    fn add_entry(&mut self, entry: LogEntry) {
-        // Maybe add verification here?
-        self.0.push(entry);
-    }
+//     fn add_entry(&mut self, entry: LogEntry) {
+//         // Maybe add verification here?
+//         self.0.push(entry);
+//     }
 
-    pub fn invalid_term_bounds(&self, index: LogIndex, term: TermId) -> bool {
-        let is_valid = self
-            .0
-            .get(index)
-            .map(|entry| entry.term == term)
-            // If the entry doesnt exist at all then we also return false
-            .unwrap_or(false);
-        !is_valid
-    }
+//     pub fn invalid_term_bounds(&self, index: LogIndex, term: TermId) -> bool {
+//         let is_valid = self
+//             .0
+//             .get(index)
+//             .map(|entry| entry.term == term)
+//             // If the entry doesnt exist at all then we also return false
+//             .unwrap_or(false);
+//         !is_valid
+//     }
 
-    pub fn append_entries(&mut self, entries: Vec<LogEntry>) -> Result<(), ()> {
-        // If an existing entry conflicts with a new one (same index
-        // but different terms), delete the existing entry and all that
-        // Append all new entries not already in the log
-        todo!()
-    }
+//     pub fn append_entries(&mut self, entries: Vec<LogEntry>) -> Result<(), ()> {
+//         // If an existing entry conflicts with a new one (same index
+//         // but different terms), delete the existing entry and all that
+//         // Append all new entries not already in the log
+//         todo!()
+//     }
 
-    pub fn get_last_index(&self) -> LogIndex {
-        self.0.last().map(|entry| entry.index).unwrap_or(0)
-    }
-}
+//     pub fn get_last_index(&self) -> LogIndex {
+//         self.0.last().map(|entry| entry.index).unwrap_or(0)
+//     }
+// }
