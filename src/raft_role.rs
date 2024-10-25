36998dbaa95df867786d33519295eee6039a06de
Jacquin Mininger <jacquin.mininger@gmail.com>
2024-10-25 16:42:59 -0400

    feat: basic election loop

---
+++ src/raft_role.rs
@@ -0,0 +1,31 @@
+use std::collections::HashMap;
+
+use tokio::sync::oneshot;
+
+use crate::{
+    peer::PeerName,
+    raft_core::LogIndex,
+};
+
+#[derive(Debug)]
+pub enum RaftRole {
+    /// The server is a follower in the Raft consensus algorithm and contains the current leaders
+    /// id
+    Follower(PeerName),
+    Candidate(CandidateState),
+    Leader(LeaderState),
+}
+
+#[derive(Debug, Clone, PartialEq, Default)]
+pub struct LeaderState {
+    next_index: HashMap<PeerName, LogIndex>,
+    match_index: HashMap<PeerName, LogIndex>, // For each known server:
+}
+
+#[derive(Debug, Default)]
+pub struct CandidateState {
+    votes: HashMap<PeerName, bool>,
+    // We call this timer every time we receive either a response with a termId higher than ours or
+    // if we receive an appendEntries with a termId higher than ours
+    // cancel_trigger: oneshot::Sender<()>,
+}
--- src/raft_state.rs
+++ src/raft_state.rs
@@ -1,6 +1,10 @@
-use std::collections::HashMap;
+use std::time::Duration;
 
 use crate::{
+    json_rpc::{
+        RpcRequest,
+        RpcResponse,
+    },
     network::NetworkManager,
     peer::PeerName,
     raft_core::{
@@ -8,65 +12,99 @@ use crate::{
         LogIndex,
         Term,
     },
-    raft_rpc::RequestVote,
+    raft_role::RaftRole,
+    raft_rpc::{
+        RequestVote,
+        RequestVoteResponse,
+    },
 };
 
-#[derive(Debug, Clone, PartialEq)]
-pub struct LeaderState {
-    next_index: HashMap<PeerName, LogIndex>,
-    match_index: HashMap<PeerName, LogIndex>, // For each known server:
-}
+const ELECTION_TIMEOUT: u64 = 5;
 
-#[derive(Debug, Clone, PartialEq)]
-pub enum ServerState {
-    Follower,
-    Candidate,
-    Leader(LeaderState),
-}
-
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 pub struct RaftState {
-    peer_name: PeerName,
+    cluster_size: usize,
+    host_name: PeerName,
+
     commit_index: LogIndex,
     last_applied: LogIndex,
-    node_type: ServerState,
+    node_type: RaftRole,
     current_term: Term,
     log: Vec<LogEntry>,
     // committed: Log,
 }
 
 impl RaftState {
-    pub fn new(peer_name: PeerName) -> Self {
+    pub fn new(peer_name: PeerName, cluster_size: usize, leader_name: PeerName) -> Self {
         RaftState {
-            peer_name,
+            cluster_size,
+            host_name: peer_name,
             log: vec![],
             commit_index: 0,
             last_applied: 0,
-            node_type: ServerState::Follower,
+            // All nodes begin as followers, but they may not necessarily know who their follower
+            // is
+            node_type: RaftRole::Follower(leader_name),
             current_term: 0,
             // log: Log::new(),
             // committed: Log::new(),
         }
     }
 
-    pub fn begin_term(&mut self, term: Term) {
-        self.current_term = term;
-        self.node_type = ServerState::Follower;
-    }
-
     fn log_index(&self) -> LogIndex {
         self.log.len() - 1
     }
 
-    pub fn initiate_election(&mut self, network: &mut NetworkManager) {
-        self.node_type = ServerState::Candidate;
+    pub fn init_election(&mut self) -> RequestVote {
+        self.node_type = RaftRole::Candidate(Default::default());
         self.current_term += 1;
         let last_log = self.log.last().cloned().unwrap_or_default();
-        let rpc_req = RequestVote {
+        RequestVote {
             term: self.current_term,
-            candidate_id: self.peer_name.clone(),
+            candidate_id: self.host_name.clone(),
             last_log_index: last_log.index,
             last_log_term: last_log.term,
-        };
+        }
+    }
+}
+
+async fn run_election(network: &mut NetworkManager, raft_state: &mut RaftState) {
+    // In the main loop check for either appends or other candidates and respond accordingly when a
+    // candidate yourself
+    let req_vote = raft_state.init_election();
+    let params = serde_json::to_value(req_vote).unwrap();
+    let rpc_req = RpcRequest {
+        id: network.next_request_id().into(),
+        method: "request_vote".to_string(),
+        params,
+    };
+    // let election_timeout = Duration::from_secs(ELECTION_TIMEOUT);
+    let rpc_timeout = Duration::from_millis(250);
+    let mut response_stream = network.broadcast(rpc_req, rpc_timeout).await;
+
+    let mut votes = 0;
+    while let Some(response) = response_stream.join_next().await {
+        let (_peer_name, result) = response.unwrap_or_default();
+        if let Some(RpcResponse {
+            result: resp_raw, ..
+        }) = result
+        {
+            if let Ok(RequestVoteResponse { term, vote_granted }) =
+                serde_json::from_value::<RequestVoteResponse>(resp_raw)
+            {
+                if term > raft_state.current_term {
+                    raft_state.current_term = term;
+                    raft_state.node_type = RaftRole::Follower(Default::default());
+                    return;
+                }
+                if vote_granted {
+                    votes += 1;
+                    if votes > raft_state.cluster_size / 2 {
+                        raft_state.node_type = RaftRole::Leader(Default::default());
+                        return;
+                    }
+                }
+            }
+        }
     }
 }
